38ab1aa575c1b251eb1075ef5d87ca765cc3cd3e
Eseo17 <CDM@cdms-mac-pro-3.campus.jcu.edu>
2018-06-13 10:24:10 -0400

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function ThisFunction(id, person) {
+    
+}
+
+
+console.log();
+
+
+// This function rocks
+
+function name(){
+    return value
+}
